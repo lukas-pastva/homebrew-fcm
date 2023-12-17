43cfea348200dff10e65b1cee629d566e49f0342
Lukas Pastva <lukas.pastva>
2023-12-17 18:25:31 +0100

    feat: initial commit

--- Formula/fcm.rb
+++ Formula/fcm.rb
@@ -1,7 +1,7 @@
 class Fcm < Formula
   desc "fcm"
   homepage "https://github.com/lukas-pastva/homebrew-fcm"
-  url "https://github.com/lukas-pastva/homebrew-fcm/releases/download/v0.1.16/fcm-v0.1.16.tar.gz"
+  url "https://github.com/lukas-pastva/homebrew-fcm/releases/download/v0.1.17/fcm-v0.1.17.tar.gz"
   sha256 ""
   license "Apache-2.0 license"
 
